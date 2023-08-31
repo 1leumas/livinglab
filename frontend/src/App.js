fcf95c79124819a2a682c138c06f014626ecf1a9
Samuel <samuelschirmer2002@gmail.com>
2023-08-31 12:56:57 -0300

    structure and connection with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://localhost:5000/api/data/nit2xli');
+      setData(result.data);
+    };
+    
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
