809df818e2256f17f6bf38a766e464ca7e24ce24
Samuel <samuelschirmer2002@gmail.com>
2023-11-21 19:41:24 -0300

    fix: error page

--- ecotech-vite/src/ui/pages/Error/styles.js
+++ ecotech-vite/src/ui/pages/Error/styles.js
@@ -5,8 +5,9 @@ export const ErrorContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: 50vh;
   width: 100vw;
   text-align: center;
   font-size: 1.5em;
+  color: black;
 `;
